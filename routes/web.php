2bc2a31b00571d83cc96d14525ee8582132263b5
fdl-11 <fadlidarusalam11@gmail.com>
2023-08-05 03:40:02 +0700

    struktur folder, routes, view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Fadli D",
+        "email" => "faldidarusalam11@gmail.com",
+        "image" => "firmino.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
