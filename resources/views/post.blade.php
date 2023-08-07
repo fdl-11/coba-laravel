12ebe6e2f8baf38ffd4382a7d0a6e3b2efa449fc
fdl-11 <fadlidarusalam11@gmail.com>
2023-08-07 21:30:46 +0700

    minor update

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
     <h1 class="mb-2">{{ $post->title }}</h1>
-    <h5 class="mb-4">by : {{ $post->author }}</h5>
+    <h5 class="mb-4">by {{ $post->author }}</h5>
     {!! $post->body !!}
 <br>
     <a href="/posts">Back to posts</a>
